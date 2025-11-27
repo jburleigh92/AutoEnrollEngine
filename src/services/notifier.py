1f40ee732aef987ccbcb3d406566e222e1f8fe1d
jburleigh92 <74757858+jburleigh92@users.noreply.github.com>
2025-11-26 19:37:19 -0800

    Create notifier.py

---
+++ src/services/notifier.py
@@ -0,0 +1,6 @@
+def send_notification(message: str) -> None:
+    """
+    Send a notification message using any configured notification service.
+    This placeholder is provided for future integrations.
+    """
+    pass
